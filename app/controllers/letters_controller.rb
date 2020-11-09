439b8609ffa2e513f4dff7e68434d714d40a68b9
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2020-11-09 16:11:36 +0900

    感謝状の詳細表示の作成

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -3,4 +3,8 @@ class LettersController < ApplicationController
   def index
     @letters = Letter.includes(:address, :user).order('created_at DESC')
   end
+
+  def show
+    @letters = Letter.includes(:address, :user).find(params[:id])
+  end
 end
