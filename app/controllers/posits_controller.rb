8daf6ed0b86cacc6310f243146c54bb5fe6f4115
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2020-11-13 19:18:11 +0900

    ビューの分岐

--- app/controllers/posits_controller.rb
+++ app/controllers/posits_controller.rb
@@ -4,6 +4,10 @@ class PositsController < ApplicationController
   def index
     @posits = Posit.includes(:user).order('created_at DESC')
     @blogs = Blog.includes(:user).order('created_at DESC')
+    likes = Like.includes([:user, :posit])
+    if user_signed_in?
+    @like = likes.where(user_id: current_user.id).count
+    end
   end
 
   def show
