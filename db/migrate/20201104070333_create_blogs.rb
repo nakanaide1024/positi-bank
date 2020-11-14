9a759f7ae974c72a37e2c8a17355a856f391475e
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2020-11-14 17:19:23 +0900

    blogsテーブルのカラム変更

--- db/migrate/20201104070333_create_blogs.rb
+++ db/migrate/20201104070333_create_blogs.rb
@@ -3,6 +3,7 @@ class CreateBlogs < ActiveRecord::Migration[6.0]
     create_table :blogs do |t|
       t.string :title, null: false, default: ""
       t.text :text, null: false 
+      t.boolean :checked
       t.references :user, null: false, foreign_key: true
       t.timestamps
     end
