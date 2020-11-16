b8211702931f67c1dbf7cda8e4ec69140241a53c
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2020-11-16 12:28:07 +0900

    アソシエーションの組み直し

--- app/models/posit.rb
+++ app/models/posit.rb
@@ -1,7 +1,7 @@
 class Posit < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
-  has_many :liked_users, through: :likes, source: :user
+  has_many :users, through: :likes
 
   validates :posit, presence: true
 
