924b2e37f300d03f98812e85dc180c04e08706b0
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2020-11-25 11:41:45 +0900

    呟きの削除機能についてのテストコードとFakerの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    nickname { 'katoudayo' }
+    nickname { Faker::Lorem.characters(number: 9) }
     email { Faker::Internet.free_email }
     password { '111aaa' }
     password_confirmation { password }
--- spec/system/posits_spec.rb
+++ spec/system/posits_spec.rb
@@ -51,3 +51,45 @@ RSpec.describe '呟きの投稿', type: :system do
     end
   end
 end
+RSpec.describe '呟きの削除', type: :system do
+  before do
+    @posit1 = FactoryBot.create(:posit)
+    @user = FactoryBot.create(:user)
+  end
+  context '呟きが削除できる時' do
+    it '自分の投稿には削除するボタンが表示され、ボタンを押すと呟きが削除される' do
+      # マイページへ移動する
+      visit posits_path
+      # 正しいユーザー情報を入力する
+      fill_in 'email', with: @posit1.user.email
+      fill_in 'password', with: @posit1.user.password
+      # ログインボタンを押す
+      find('input[name="commit"]').click
+      # 自分の投稿と削除ボタンがあることを確認する
+      expect(
+        all(".posit")[0].hover
+      ).to have_link "削除する", href: posit_path(@posit1)
+      # 削除ボタンを押した後、削除しますか？というボタンをクリックすると、その呟きが消え、positsテーブルのレコードが一つ減ったことが確認できる
+      expect{
+        all(".posit")[0].hover.find_link("削除する", href: posit_path(@posit1)).click
+        expect(page.accept_confirm).to eq "削除しますか？"
+        expect(page).to have_no_content("#{@posit1.posit}")
+      }.to change { Posit.count }.by(-1)
+    end
+  end
+  context '呟きが削除できない時' do
+    it '呟きがあっても自分の投稿ではない時は削除できない' do
+      # マイページへ移動する
+      visit posits_path
+      # 投稿とは別のユーザー情報を入力する
+      fill_in 'email', with: @user.email
+      fill_in 'password', with: @user.password
+      # ログインボタンを押す
+      find('input[name="commit"]').click
+      # 投稿はあるが削除ボタンがないことを確認する
+      expect(
+        all(".posit")[0].hover
+      ).to have_no_link "削除する", href: posit_path(@posit1)
+    end
+  end
+end
