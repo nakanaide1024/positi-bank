9ef4a601d435d36cba9571efee01bfee8e6138e2
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2020-11-28 20:10:12 +0900

    profileモデルの単体テストコード

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @profile = FactoryBot.build(:profile)
+  end
+  describe 'profileの保存' do
+    context 'profileの保存が上手く行く時' do
+      it '正しく入力されていれば保存される' do
+        expect(@profile).to be_valid
+      end
+      it '何も入力されなくても登録できる' do
+        @profile.profile = ''
+        expect(@profile).to be_valid
+      end
+    end
+  end
 end
