44840755bba601450e876656a9c796d91468b87a
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2020-11-29 22:14:09 +0900

    rubocopの実装

--- spec/system/blogs_spec.rb
+++ spec/system/blogs_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "ブログの作成", type: :system do
+RSpec.describe 'ブログの作成', type: :system do
   before do
     @blog = FactoryBot.create(:blog)
   end
@@ -14,7 +14,7 @@ RSpec.describe "ブログの作成", type: :system do
       # ログインボタンを押す
       find('input[name="commit"]').click
       # ヘッダーに記事を書くボタンがあることを確認する
-      expect(page).to have_content("記事を書く")
+      expect(page).to have_content('記事を書く')
       # ブログ作成ページへ遷移する
       visit new_blog_path
       # 記事内容を入力する
@@ -23,11 +23,11 @@ RSpec.describe "ブログの作成", type: :system do
       # 投稿ボタンを押すとblogsテーブルのカウントが一つ上がる
       expect do
         find('input[name="commit"]').click
-      end.to change{ Blog.count }.by(1)
+      end.to change { Blog.count }.by(1)
       # マイページへ遷移したことを確認する
       expect(current_path).to eq posits_path
       # マイページに作成したブログのタイトルが表示されている
-      expect(page).to have_content("#{@blog.title}")
+      expect(page).to have_content(@blog.title.to_s)
     end
   end
   context 'ブログの作成ができない時' do
@@ -40,16 +40,16 @@ RSpec.describe "ブログの作成", type: :system do
       # ログインボタンを押す
       find('input[name="commit"]').click
       # ヘッダーに記事を書くボタンがあることを確認する
-      expect(page).to have_content("記事を書く")
+      expect(page).to have_content('記事を書く')
       # ブログ作成ページへ遷移する
       visit new_blog_path
       # 記事内容を入力する
-      fill_in 'blog_title', with: ""
-      fill_in 'blog_text', with: ""
+      fill_in 'blog_title', with: ''
+      fill_in 'blog_text', with: ''
       # 投稿ボタンを押す
       find('input[name="commit"]').click
       # ブログ作成ページに再び遷移する
-      expect(current_path).to eq "/blogs"
+      expect(current_path).to eq '/blogs'
     end
   end
 end
@@ -57,7 +57,6 @@ RSpec.describe 'ブログの編集', type: :system do
   before do
     @blog = FactoryBot.create(:blog)
     @blog2 = FactoryBot.create(:blog)
-
   end
   context 'ブログが編集できる時' do
     it '自分のブログの詳細ページに遷移すると、編集ボタンが表示され、ボタンを押すと編集ページに遷移し編集できる' do
@@ -120,15 +119,15 @@ RSpec.describe 'ブログの編集', type: :system do
       # ログインボタンを押す
       find('input[name="commit"]').click
       # 自分以外のユーザーが作成したブログを選択する
-      all(".blog")[0].hover.find_link(@blog2.title.to_s, href: blogs_path(@blog2)).click
+      all('.blog')[0].hover.find_link(@blog2.title.to_s, href: blogs_path(@blog2)).click
       # 選択したブログの詳細にとぶ
       visit blogs_path(@blog2)
       # 編集ボタンがないことを確認する
-      expect(page).to have_no_content("編集する")
+      expect(page).to have_no_content('編集する')
     end
   end
 end
-RSpec.describe "ブログの削除", type: :system do
+RSpec.describe 'ブログの削除', type: :system do
   before do
     @blog = FactoryBot.create(:blog)
     @blog2 = FactoryBot.create(:blog)
@@ -148,7 +147,7 @@ RSpec.describe "ブログの削除", type: :system do
       visit blogs_path(@blog)
       # 削除ボタンを押しダイアログのOKを押すと、ブログテーブルのレコードが一つ減り、マイページへ遷移する
       expect do
-        find_link("削除する", href: blog_path(@blog)).click
+        find_link('削除する', href: blog_path(@blog)).click
         expect(page.accept_confirm).to eq '削除しますか？'
         expect(page).to have_no_content('削除しますか？')
       end.to change { Blog.count }.by(-1)
@@ -168,11 +167,11 @@ RSpec.describe "ブログの削除", type: :system do
       # ログインボタンを押す
       find('input[name="commit"]').click
       # 他のユーザーのブログのタイトルをクリックする
-      all(".blog")[0].hover.find_link(@blog2.title.to_s, href: blogs_path(@blog2)).click 
+      all('.blog')[0].hover.find_link(@blog2.title.to_s, href: blogs_path(@blog2)).click
       # ブログ詳細に遷移する
       visit blogs_path(@blog2)
       # 削除ボタンがないことを確認する
-      expect(page).to have_no_content("削除する")
+      expect(page).to have_no_content('削除する')
     end
   end
 end
