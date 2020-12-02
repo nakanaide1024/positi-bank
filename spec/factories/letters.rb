d896b33adc4da06e4b8ca4597ccf5e286ee1b2a8
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2020-12-02 19:13:05 +0900

    lettersテーブルの単体テストコード

--- spec/factories/letters.rb
+++ spec/factories/letters.rb
@@ -1,4 +1,6 @@
 FactoryBot.define do
   factory :letter do
+    thanks { Faker::Lorem.sentence }
+    association :user
   end
 end
--- spec/models/letter_spec.rb
+++ spec/models/letter_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Letter, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @thanks = FactoryBot.build(:letter)
+  end
+  describe '感謝状の保存' do
+    context '感謝状が保存できる時' do
+      it '感謝状の内容が入力されていれば保存できる' do
+        expect(@thanks).to be_valid
+      end
+    end
+    context '感謝状が保存できない時' do
+      it '感謝状の内容が入力されていなければ保存できない' do
+        @thanks.thanks = ''
+        @thanks.valid?
+        expect(@thanks.errors.full_messages).to include("Thanks can't be blank")
+      end
+    end
+  end
 end
